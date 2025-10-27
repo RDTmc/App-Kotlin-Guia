9ece682ebf6675010c00921d84fecf7213b2ef2f
RDTmc <rd.munoz@duocuc.cl>
2025-10-27 00:49:35 -0300

    Funcionalidad cámara y galería integrada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.9.5")
     implementation("androidx.datastore:datastore-preferences:1.1.7")
     implementation(libs.androidx.datastore.preferences.core)
+    implementation(libs.androidx.room.ktx)
 
 
 
--- app/src/main/java/com/milsabores/appkotlin_guia/viewmodel/PerfilViewModel.kt
+++ app/src/main/java/com/milsabores/appkotlin_guia/viewmodel/PerfilViewModel.kt
@@ -1,2 +1,39 @@
 package com.milsabores.appkotlin_guia.viewmodel
 
+import android.app.Application
+import android.content.Context
+import android.net.Uri
+import androidx.core.content.FileProvider
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.io.File
+
+class PerfilViewModel(app: Application) : AndroidViewModel(app) {
+
+    private val _fotoUri = MutableStateFlow<Uri?>(null)
+    val fotoUri: StateFlow<Uri?> = _fotoUri
+
+    // Galeria
+    fun setFromGallery(uri: Uri) {
+        _fotoUri.value = uri
+    }
+
+    // Camara: crea un archivo temporal y devuelve su Uri para TakePicture()
+    fun createTempImageUri(): Uri {
+        val ctx: Context = getApplication()
+        val imagesDir = File(ctx.cacheDir, "images").apply { mkdirs() }
+        val imageFile = File.createTempFile("captura_", ".jpg", imagesDir)
+        return FileProvider.getUriForFile(ctx, "${ctx.packageName}.fileprovider", imageFile)
+    }
+
+    // Camara: confirmar captura exitosa
+    fun setFromCamera(success: Boolean, outputUri: Uri?) {
+        if (success) _fotoUri.value = outputUri
+    }
+
+    // limpiar
+    fun clear() { viewModelScope.launch { _fotoUri.value = null } }
+}
