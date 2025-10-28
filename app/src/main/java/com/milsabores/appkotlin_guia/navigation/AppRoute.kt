46ed90467d6366f6d503abeaa98418095265b874
RDTmc <rd.munoz@duocuc.cl>
2025-10-28 10:00:48 -0300

    feat(nav): rutas Splash, Onboarding y Entry

--- app/src/main/java/com/milsabores/appkotlin_guia/navigation/AppRoute.kt
+++ app/src/main/java/com/milsabores/appkotlin_guia/navigation/AppRoute.kt
@@ -1,6 +1,10 @@
 package com.milsabores.appkotlin_guia.navigation
 
 sealed class AppRoute(val route:String) {
+
+    data object Splash     : AppRoute("splash")
+    data object Onboarding : AppRoute("onboarding")
+    data object Entry      : AppRoute("entry")
     data object Home:AppRoute("home")
     data object Register: AppRoute("register")
     data object Profile: AppRoute("profile")
