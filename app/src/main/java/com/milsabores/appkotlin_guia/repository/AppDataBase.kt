b5a92ddeaad9f7389be47ccf236a2c62ba76ec0a
RDTmc <rd.munoz@duocuc.cl>
2025-10-31 06:22:10 -0300

    feat(db/orders): tabla de órdenes en Room

--- app/src/main/java/com/milsabores/appkotlin_guia/repository/AppDataBase.kt
+++ app/src/main/java/com/milsabores/appkotlin_guia/repository/AppDataBase.kt
@@ -6,18 +6,21 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.milsabores.appkotlin_guia.model.Users
 import com.milsabores.appkotlin_guia.model.CartEntity
+import com.milsabores.appkotlin_guia.model.OrderEntity
 
 @Database(
     entities = [
         Users::class,
-        CartEntity::class,      // 👈 nueva
+        CartEntity::class,
+        OrderEntity::class,
     ],
-    version = 2,               // 👈 súbelo 1
+    version = 3,               // 👈 súbelo 1
     exportSchema = false
 )
 abstract class AppDataBase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun cartDao(): CartDao
+    abstract fun orderDao(): OrderDao
 
     companion object {
         @Volatile private var INSTANCE: AppDataBase? = null
