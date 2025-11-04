40a7ef23e1a6d51f2530e6cfd4039d214d38726f
RDTmc <rd.munoz@duocuc.cl>
2025-11-03 22:11:48 -0300

    style(theme): Añadir definición de fuente Pacifico para branding

--- app/src/main/java/com/milsabores/appkotlin_guia/ui/theme/Type.kt
+++ app/src/main/java/com/milsabores/appkotlin_guia/ui/theme/Type.kt
@@ -2,9 +2,16 @@ package com.milsabores.appkotlin_guia.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import com.milsabores.appkotlin_guia.R
 import androidx.compose.ui.unit.sp
+
+val Pacifico = FontFamily(
+    Font(R.font.pacifico_regular, FontWeight.Normal)
+)
+
 
 val AppTypography = Typography(
     headlineSmall = TextStyle(
